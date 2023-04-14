593435a36a630b63bf2b415f5089c6b5a9410951
LLLida <0adilmohammad0@gmail.com>
2023-04-14 15:57:04 +0300

    Serialization support for previous commit
    It was very easy, but I don't like that each time we introduce some
    change in data we need to rewrite serialization code. I'll certainly
    use some other language in conjunction in C after the presentation.
    
    Anyway we can duplicate voxel models on screen without duplicating
    them in memory which is great 🍰

--- src/lida_package.c
+++ src/lida_package.c
@@ -14,16 +14,17 @@ typedef struct {
   Vec3 camera_up;
   Vec3 camera_rotation;
 
-  uint32_t vox_components_offset;
-  uint32_t num_vox_components;
+  uint32_t vox_grids_offset;
+  uint32_t num_vox_grids;
+  uint32_t vox_models_offset;
+  uint32_t num_vox_models;
 
 } Scene_Info;
 
 typedef struct {
 
   Transform transform;
-  uint32_t palette[256];
-  uint32_t w, h, d;
+  uint32_t grid_id;
   int has_script;
 
 } Vox_Model_Serialized;
@@ -39,42 +40,56 @@ typedef struct {
 
 } Script_Serialized;
 
+typedef struct {
+
+  uint32_t palette[256];
+  uint32_t w, h, d;
+
+} Vox_Grid_Serialized;
+
 INTERNAL void
 SaveScene(const Camera* camera, const char* filename)
 {
   void* file = PlatformOpenFileForWrite(filename);
 
   // write header
-  uint64_t header = PACKAGE_MAGIC;
+  const uint64_t header = PACKAGE_MAGIC;
   PlatformWriteToFile(file, &header, sizeof(header));
 
   Scene_Info info = {
     .camera_position = camera->position,
     .camera_up       = camera->up,
     .camera_rotation = camera->rotation,
-    .num_vox_components = ComponentCount(Voxel_Grid),
-    .vox_components_offset = sizeof(Scene_Info) + sizeof(uint64_t)
+    .num_vox_models = ComponentCount(Voxel_View),
+    .vox_models_offset = sizeof(Scene_Info) + sizeof(uint64_t),
+    .num_vox_grids = ComponentCount(Voxel_Grid),
+    .vox_grids_offset = sizeof(Scene_Info) + sizeof(uint64_t)
   };
+  FOREACH_COMPONENT(Voxel_View) {
+    (void)components[i]; // HACK: avoid compiler warning
+    Script* script = GetComponent(Script, entities[i]);
+    info.vox_grids_offset += sizeof(Vox_Model_Serialized);
+    if (script) {
+      info.vox_grids_offset += sizeof(Script_Serialized);
+    }
+  }
   PlatformWriteToFile(file, &info, sizeof(Scene_Info));
 
-  EID* ids = ComponentIDs(Voxel_Grid);
-  for (uint32_t i = 0; i < info.num_vox_components; i++) {
+  EID* ids = ComponentIDs(Voxel_View);
+  for (uint32_t i = 0; i < info.num_vox_models; i++) {
     EID entity = ids[i];
-    Voxel_Grid* grid = GetComponent(Voxel_Grid, entity);
+    Voxel_View* vox = GetComponent(Voxel_View, entity);
     Transform* transform = GetComponent(Transform, entity);
     Script* script = GetComponent(Script, entity);
     int has_script = script ? 1 : 0;
-
+    Voxel_Grid* grid = GetComponent(Voxel_Grid, vox->grid);
     Vox_Model_Serialized model = {
-      .w = grid->width,
-      .h = grid->height,
-      .d = grid->depth,
+      .grid_id = grid - ComponentData(Voxel_Grid),
       .has_script = has_script
     };
-    memcpy(model.palette, grid->palette, sizeof(uint32_t)*256);
     memcpy(&model.transform, transform, sizeof(Transform));
-
     PlatformWriteToFile(file, &model, sizeof(Vox_Model_Serialized));
+
     if (has_script) {
       Script_Serialized ss = {
         .arg0 = script->arg0,
@@ -86,6 +101,18 @@ SaveScene(const Camera* camera, const char* filename)
       strncpy(ss.name, script->name, sizeof(ss.name)-1);
       PlatformWriteToFile(file, &ss, sizeof(Script_Serialized));
     }
+  }
+  ids = ComponentIDs(Voxel_Grid);
+  for (uint32_t i = 0; i < info.num_vox_grids; i++) {
+    EID entity = ids[i];
+    Voxel_Grid* grid = GetComponent(Voxel_Grid, entity);
+    Vox_Grid_Serialized grid_info = {
+      .w = grid->width,
+      .h = grid->height,
+      .d = grid->depth,
+    };
+    memcpy(grid_info.palette, grid->palette, sizeof(uint32_t)*256);
+    PlatformWriteToFile(file, &grid_info, sizeof(Vox_Grid_Serialized));
     PlatformWriteToFile(file, grid->data->ptr, VoxelGridBytes(grid));
     // TODO: pad to 8 or 16 bytes
   }
@@ -98,7 +125,8 @@ INTERNAL void
 LoadScene(ECS* ecs, Allocator* va, Camera* camera, Script_Manager* sm, const char* filename)
 {
   size_t buffer_size;
-  // TODO: packages might become very big, we should read directly from file
+  // TODO: packages might become very big, we should read directly
+  // from file, i.e. without any buffers.
   void* buffer = PlatformLoadEntireFile(filename, &buffer_size);
   if (buffer == NULL) {
     LOG_ERROR("failed to load package '%s'", filename);
@@ -112,32 +140,31 @@ LoadScene(ECS* ecs, Allocator* va, Camera* camera, Script_Manager* sm, const cha
     camera->up       = info->camera_up;
     camera->rotation = info->camera_rotation;
 
-    Vox_Model_Serialized* model = (void*)((uint8_t*)buffer + info->vox_components_offset);
-    for (uint32_t i = 0; i < info->num_vox_components; i++) {
+    EID* grid_ids = PersistentAllocate(sizeof(EID) * info->num_vox_grids);
+
+    Vox_Model_Serialized* model = (void*)((uint8_t*)buffer + info->vox_models_offset);
+    Vox_Grid_Serialized* grid = (void*)((uint8_t*)buffer + info->vox_grids_offset);
+
+    // create entities for grids
+    for (uint32_t i = 0; i < info->num_vox_grids; i++)
+      grid_ids[i] = CreateEntity(ecs);
+    // create Voxel_View's
+    for (uint32_t i = 0; i < info->num_vox_models; i++) {
       EID entity = CreateEntity(ecs);
 
-#if VX_USE_BLOCKS
-      uint32_t model_data_bytes = ALIGN_TO(model->w, 4)*ALIGN_TO(model->h, 4)*ALIGN_TO(model->d, 4);
-#else
-      uint32_t model_data_bytes = model->w*model->h*model->d;
-#endif
-
-      Voxel_Grid* vox = AddComponent(ecs, Voxel_Grid, entity);
-      // load palette
-      memcpy(vox->palette, model->palette, sizeof(uint32_t)*256);
-      AllocateVoxelGrid(va, vox, model->w, model->h, model->d);
+      Voxel_View* vox = AddComponent(ecs, Voxel_View, entity);
+      vox->first_vertex = UINT32_MAX;
+      vox->grid = grid_ids[model->grid_id];
 
       // load transform
       Transform* transform = AddComponent(ecs, Transform, entity);
       memcpy(transform, &model->transform, sizeof(Transform));
       AddComponent(ecs, OBB, entity);
 
-      // 'load' script if this component has one
-      void* model_data = model+1;
       if (model->has_script) {
         // NOTE: we already have script in memory because 'scripts'
         // are functions in C. We just do a hash table lookup to
-        // retrieve needed pointers.
+        // retrieve needed function pointers.
         Script_Serialized* info = (void*)((uint8_t*)(model + 1));
         Script* script = AddComponent(ecs, Script, entity);
         Script_Entry* entry = FHT_Search(&sm->scripts, GET_TYPE_INFO(Script_Entry),
@@ -149,17 +176,24 @@ LoadScene(ECS* ecs, Allocator* va, Camera* camera, Script_Manager* sm, const cha
         script->arg2 = info->arg2;
         script->arg3 = info->arg3;
         script->frequency = info->frequency;
-
-        model_data = (uint8_t*)model + sizeof(Vox_Model_Serialized) + sizeof(Script_Serialized);
+        model = (void*)((uint8_t*)model + sizeof(Vox_Model_Serialized) + sizeof(Script));
+      } else {
+        model = (void*)((uint8_t*)model + sizeof(Vox_Model_Serialized));
       }
+    }
+    // load voxels
+    for (uint32_t i = 0; i < info->num_vox_grids; i++) {
+      Voxel_Grid* vox = AddComponent(ecs, Voxel_Grid, grid_ids[i]);
+      // load palette
+      memcpy(vox->palette, grid->palette, sizeof(uint32_t)*256);
+      AllocateVoxelGrid(va, vox, grid->w, grid->h, grid->d);
 
-      // load voxel data
-      memcpy(vox->data->ptr, model_data, model_data_bytes);
+      memcpy(vox->data->ptr, grid+1, VoxelGridBytes(vox));
       RehashVoxelGrid(vox);
-
-      model = (void*)((uint8_t*)model_data + model_data_bytes);
+      grid = (void*)((uint8_t*)(grid +1) + VoxelGridBytes(vox));
     }
 
+    PersistentRelease(grid_ids);
   }
   PlatformFreeLoadedFile(buffer);
   LOG_INFO("Loaded scene from file '%s'", filename);
